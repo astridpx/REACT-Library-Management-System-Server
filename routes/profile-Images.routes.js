c6d32a8662df3812e85c4eff7d7f6842533329de
Iskyy <iskycruise01@gmail.com>
2023-02-16 08:59:30 +0700

    supply api key error

--- routes/profile-Images.routes.js
+++ routes/profile-Images.routes.js
@@ -8,6 +8,12 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 const path = require("path");
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
 const storage = multer.diskStorage({
   // destination: (req, file, cb) => {
   //   // cb(null, "./Images");
