aae12a00d4394cc3c7577bc78b74b64d77f7389c
Iskyy <iskycruise01@gmail.com>
2023-02-16 09:19:45 +0700

    Fixing cors policy

--- server.js
+++ server.js
@@ -33,6 +33,14 @@ app.use(
     credentials: true,
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // app.use(cookieParser());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(
