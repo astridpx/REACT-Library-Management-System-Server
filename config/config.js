9df27ce685a95bd52907fba952f5fb0730417a42
Iskyy <iskycruise01@gmail.com>
2023-03-25 08:11:17 +0700

    SQL connecctionlimit back

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  // connectionLimit: 1000,
-  // connectTimeout: 60 * 60 * 1000,
-  // acquireTimeout: 60 * 60 * 1000,
-  // timeout: 60 * 60 * 1000,
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
   user: process.env.USER1,
   host: process.env.HOST1,
   password: process.env.DBPASSWORD1,
